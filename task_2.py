332237cf4cbd4fe579bb30b004f07198c91df680
Abibulla-23 <Abibamad@gmail.com>
2024-05-04 18:34:07 +0300

    Master

--- task_2.py
+++ task_2.py
@@ -1,3 +1,8 @@
 player = 'X'
-board =
+board = [[' 1', '1 ', ' 1'], [' ', ' ', ' '], [' ', ' ', ' ']]
 def ask_and_make_move(player, board):
+    move1 = input('Введите вертикальные координаты:')
+    move2 = input('Введите горизонтальные координаты:')
+    return
+
+print(board[0][0])
