fd0b07fb09a05684fa7e4ff51f4539377ff30bc9
Abibulla-23 <Abibamad@gmail.com>
2024-05-04 18:58:40 +0300

    Master

--- task_2.py
+++ task_2.py
@@ -1,8 +1,12 @@
 player = 'X'
 board = [[' 1', '1 ', ' 1'], [' ', ' ', ' '], [' ', ' ', ' ']]
-def ask_and_make_move(player, board):
-    move1 = input('Введите вертикальные координаты:')
-    move2 = input('Введите горизонтальные координаты:')
-    return
 
-print(board[0][0])
+
+def ask_and_make_move(player, board):
+    point1 = int(input('Введите вертикальные координаты:'))
+    point2 = int(input('Введите горизонтальные координаты:'))
+    move = board[point1-1][point2-1]
+
+
+ask_and_make_move(player, board)
+
