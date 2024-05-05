02eccc7007e397c3f1649e1e8a2c448c9cf1decd
Abibulla-23 <Abibamad@gmail.com>
2024-05-05 12:42:00 +0300

    Making moves

--- task_2.py
+++ task_2.py
@@ -1,12 +1,30 @@
-player = 'X'
-board = [[' 1', '1 ', ' 1'], [' ', ' ', ' '], [' ', ' ', ' ']]
+player1 = 'X'
+player2 = '0'
+board = [[' ', ' ', ' '], [' ', ' ', ' '], [' ', ' ', ' ']]
 
 
 def ask_and_make_move(player, board):
     point1 = int(input('Введите вертикальные координаты:'))
     point2 = int(input('Введите горизонтальные координаты:'))
     move = board[point1-1][point2-1]
+    if move == ' ':
+        board[point1-1][point2-1] = player
+        print(board)
+    else:
+        ask_and_make_move(player, board)
 
 
-ask_and_make_move(player, board)
-
+ask_and_make_move(player1, board)
+ask_and_make_move(player2, board)
+ask_and_make_move(player1, board)
+ask_and_make_move(player2, board)
+ask_and_make_move(player1, board)
+ask_and_make_move(player2, board)
+ask_and_make_move(player1, board)
+ask_and_make_move(player2, board)
+ask_and_make_move(player1, board)
+ask_and_make_move(player2, board)
+ask_and_make_move(player1, board)
+ask_and_make_move(player2, board)
+ask_and_make_move(player1, board)
+ask_and_make_move(player2, board)
