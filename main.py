69a204006f92fbc02a4bcd3e0ed25869ffe959df
Abibulla-23 <Abibamad@gmail.com>
2024-05-05 16:37:01 +0300

    Making moves

--- main.py
+++ main.py
@@ -12,6 +12,11 @@ player1 = 'X'
 player2 = '0'
 
 
+def win(check_board):
+    if check_board[0] == ['X', 'X', 'X'] or check_board[1] == ['X', 'X', 'X'] or check_board[2] == ['X', 'X', 'X']:
+        print('Крестики победили!')
+
+
 def ask_and_make_move(player, board):
     point1 = int(input('Введите вертикальные координаты:'))
     point2 = int(input('Введите горизонтальные координаты:'))
@@ -19,6 +24,7 @@ def ask_and_make_move(player, board):
     if move == ' ':
         board[point1-1][point2-1] = player
         draw_board(board)
+        win(board)
     else:
         ask_and_make_move(player, board)
 
@@ -37,3 +43,5 @@ ask_and_make_move(player1, board)
 ask_and_make_move(player2, board)
 ask_and_make_move(player1, board)
 ask_and_make_move(player2, board)
+
+
