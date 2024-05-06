5b047898c1c46bab8b8a2120754092f23f25305f
Abibulla-23 <Abibamad@gmail.com>
2024-05-06 16:22:13 +0300

    Finished move pattern

--- task_3.py
+++ task_3.py
@@ -1,1 +1,44 @@
+board = [[' ', ' ', ' '], [' ', ' ', ' '], [' ', ' ', ' ']]
+player1 = 'X'
+player2 = '0'
 
+
+def draw_board(board):
+    for i in range(3):
+        print(" | ".join(board[i]))
+        print("---------")
+
+
+def ask_move(player, board):
+    point1 = int(input('Введите вертикальные координаты:'))
+    point2 = int(input('Введите горизонтальные координаты:'))
+    sign_of_player = player
+    return sign_of_player, point1, point2
+
+
+def make_move(player, board, x, y):
+    if board[x-1][y-1] == ' ':
+        board[x-1][y-1] = player
+        draw_board(board)
+    else:
+        ask_and_make_move(player, board)
+
+
+def ask_and_make_move(player, board):
+    tuple_with_move = ask_move(player, board)
+    sign_of_player = tuple_with_move[0]
+    point1 = tuple_with_move[1]
+    point2 = tuple_with_move[2]
+    make_move(sign_of_player, board, point1, point2)
+
+
+def win(check_board):
+    if check_board[0] == ['X', 'X', 'X'] or check_board[1] == ['X', 'X', 'X'] or check_board[2] == ['X', 'X', 'X']:
+        print('Крестики победили!')
+
+
+draw_board(board)
+
+ask_and_make_move(player1, board)
+ask_and_make_move(player1, board)
+ask_and_make_move(player1, board)
